84e4d700a7d0f8ace2731f7f341b1e98e8f2dea2
jessicalam <j.lam@mail.com>
2020-01-08 12:34:41 -0500

    colors

--- colors.js
+++ colors.js
@@ -6,7 +6,11 @@ module.exports = {
   primaryLight: '#ae66df',
   primaryDark: '#4b007d',
 
-  secondary: '#ff4081',
-  secondaryLight: '#ff79b0',
-  secondaryDark: '#c60055',
+  secondary: '#9AF0C1',
+  secondaryLight: '#D6FFD8',
+  secondaryDark: '#3EB489',
 };
+
+// secondary: '#ff4081',
+//     secondaryLight: '#ff79b0',
+//     secondaryDark: '#c60055',
